7a1f8737fb33eacbf775200eefa1ad10a013bc2c
Charles Woods <charles.woods48@yahoo.com>
2022-03-15 13:22:31 -0500

    modals kinda working, need to rework after lunch

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+// import LoginForm from './components/auth/LoginForm';
+// import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+import Splash from './components/SplashPage'
 
 function App() {
   const [loaded, setLoaded] = useState(false);
+  const sessionUser = useSelector(state => state.session.user)
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -26,22 +28,16 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
+      {sessionUser && <NavBar user={sessionUser}/>}
       <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
+        <Route path='/' exact={true} >
+          <Splash />
         </Route>
         <ProtectedRoute path='/users' exact={true} >
           <UsersList/>
         </ProtectedRoute>
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
---
+++ react-app/src/components/SplashPage.js
@@ -0,0 +1,28 @@
+import LoginModal from './modals/LoginModal';
+import SignupModal from './modals/SignupModal'
+import { useSelector } from "react-redux";
+import { Redirect } from 'react-router-dom';
+import DemoUser from './auth/DemoUser';
+
+function Splash() {
+    const sessionUser = useSelector((state) => state.session.user);
+
+    return (
+        <div style={{backgroundColor: "black"}} className='splash-container'>
+            {/* {sessionUser ? (<Redirect to='/users' />) : */}
+                (<div className="splash-main">
+                    <div className="splash-text">
+                        <h1 className="splash-title">Welcome to One Mile More</h1>
+                    </div>
+                    <div className="splash-buttons">
+                        <LoginModal />
+                        <SignupModal />
+                        <DemoUser />
+                    </div>
+                </div>)
+            {/* } */}
+        </div>
+    )
+}
+
+export default Splash;
