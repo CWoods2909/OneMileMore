ae277dba8a2b61189420d2e67e6097a30cc7bd1e
Charles Woods <charles.woods48@yahoo.com>
2022-03-15 15:36:25 -0500

    splash page structure done, login validation done, modals all working

--- react-app/src/components/SplashPage.js
+++ react-app/src/components/SplashPage.js
@@ -3,13 +3,13 @@ import SignupModal from './modals/SignupModal'
 import { useSelector } from "react-redux";
 import { Redirect } from 'react-router-dom';
 import DemoUser from './auth/DemoUser';
+import './Splash.css'
 
 function Splash() {
     const sessionUser = useSelector((state) => state.session.user);
-
     return (
         <div style={{backgroundColor: "black"}} className='splash-container'>
-            {/* {sessionUser ? (<Redirect to='/users' />) : */}
+            {sessionUser ? (<Redirect to='/users' />) :
                 (<div className="splash-main">
                     <div className="splash-text">
                         <h1 className="splash-title">Welcome to One Mile More</h1>
@@ -20,7 +20,7 @@ function Splash() {
                         <DemoUser />
                     </div>
                 </div>)
-            {/* } */}
+            }
         </div>
     )
 }
