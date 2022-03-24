cbb1ad7f49da99af95a0e2be045ac25f6f6b6330
Charles Woods <charles.woods48@yahoo.com>
2022-03-24 18:04:37 -0500

    updated buttons

--- react-app/src/components/modals/DeleteCommentModal.js
+++ react-app/src/components/modals/DeleteCommentModal.js
@@ -1,12 +1,13 @@
 import { FormModal } from '../../context/Modal';
 import { useState } from 'react';
 import DestroyCommentForm from '../forms/DeleteComment'
+import { FaTrashAlt } from "react-icons/fa";
 
 const DeleteComment = ({comment}) => {
     const [ renderModal, setRenderModal ] = useState(false);
     return (
         <>
-            <button className='delete-button-comment' onClick={() => setRenderModal(true)}>Delete</button>
+            <button className='delete-button-comment' onClick={() => setRenderModal(true)}><FaTrashAlt className='trash'/></button>
             {renderModal ? (
                 <FormModal onClose={() => setRenderModal(false)}>
                     <DestroyCommentForm  comment={comment} onClose={() => setRenderModal(false)}/>
--- react-app/src/components/modals/EditCommentModal.js
+++ react-app/src/components/modals/EditCommentModal.js
@@ -1,13 +1,15 @@
 import EditCommentForm from '../forms/EditCommentForm';
 import { FormModal } from '../../context/Modal';
 import { useState } from 'react';
+import { FaPencilAlt } from "react-icons/fa";
+
 
 const EditComment = ({comment}) => {
     const [renderModal, setRenderModal] = useState(false);
 
     return (
         <div>
-            <button className='edit-comment-button' onClick={() => setRenderModal(true)}>Edit</button>
+            <button className='edit-comment-button' onClick={() => setRenderModal(true)}><FaPencilAlt className='pencil'/></button>
             {renderModal ? (
                 <FormModal onClose={() => setRenderModal(false)}>
                     <EditCommentForm comment={comment} onClose={() => setRenderModal(false)} />
